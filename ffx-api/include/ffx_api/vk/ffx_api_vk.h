3ca5cad5f46988dca365923ddbb5919b2168a79d
cdozdil <cdozdil@gmail.com>
2025-01-18 22:53:53 +0300

    rename VK enum

--- ffx-api/include/ffx_api/vk/ffx_api_vk.h
+++ ffx-api/include/ffx_api/vk/ffx_api_vk.h
@@ -102,7 +102,7 @@ struct ffxConfigureDescFrameGenerationSwapChainKeyValueVK
 //enum value matches enum FfxFrameInterpolationSwapchainConfigureKey
 enum FfxApiConfigureFrameGenerationSwapChainKeyVK
 {
-    FFX_API_CONFIGURE_FG_SWAPCHAIN_KEY_WAITCALLBACK = 0                     ///< Sets FfxWaitCallbackFunc
+    FFX_API_CONFIGURE_FG_SWAPCHAIN_KEY_WAITCALLBACK_VK = 0                     ///< Sets FfxWaitCallbackFunc
 };
 
 /// Function to get the number of presents. This is useful when using frame interpolation
